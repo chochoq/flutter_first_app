6f1eaac3dd31f2018582560b25f8fa8514a242b5
chochoq <aubepe@gmail.com>
2021-09-12 23:58:52 +0900

    (add) Text

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'character card',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHome(),
+      title: 'card',
+      home: Grade(),
     );
   }
 }
 
-class MyHome extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green[300],
       appBar: AppBar(
-        title: Text('Card'),
+        title: Text('card'),
+        backgroundColor: Colors.green[900],
         centerTitle: true,
-        backgroundColor: Colors.orange,
         elevation: 0.0,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('dsd'),
-            Text('dsd'),
-            Text('dsd'),
-            Text('dsd'),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'character',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Character POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
