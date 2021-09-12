d5461ab380022bae4b8d298c44fe9263cdce157a
chochoq <aubepe@gmail.com>
2021-09-12 22:03:03 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter first app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Hi'),
+            Text('Hi'),
+            Text('Hi'),
+          ],
+        ),
+      ),
+    );
+  }
+}
