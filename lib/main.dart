834bf66664b813f038457ca667749634be828080
chochoq <aubepe@gmail.com>
2021-09-26 20:56:18 +0900

    (add) img

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'card',
       home: Grade(),
     );
@@ -27,7 +28,19 @@ class Grade extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/1.png'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+              thickness: 0.6,
+              endIndent: 30.0,
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -67,6 +80,61 @@ class Grade extends StatelessWidget {
                   fontSize: 28.0,
                   fontWeight: FontWeight.bold),
             ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Text('face hero tattoo',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    )),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/2.png'),
+                radius: 40.0,
+                backgroundColor: Colors.green[800],
+              ),
+            )
           ],
         ),
       ),
