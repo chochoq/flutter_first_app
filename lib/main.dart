91bcd2163fd50f72717573d36648355b67154fb3
chochoq <aubepe@gmail.com>
2022-01-20 22:47:32 +0900

    (add) 스낵바

--- lib/main.dart
+++ lib/main.dart
@@ -20,86 +20,34 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("my page"),
-        centerTitle: true,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('dd');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('dd');
-            },
-          )
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('chocho'),
-              accountEmail: Text('mail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/1.png'),
-                backgroundColor: Colors.white,
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                    backgroundImage: AssetImage('assets/1.png'), backgroundColor: Colors.white),
-                // CircleAvatar(
-                //     backgroundImage: AssetImage('assets/1.png'), backgroundColor: Colors.white),
-              ],
-              onDetailsPressed: () {
-                print('dd');
-              },
-              decoration: BoxDecoration(
-                  color: Colors.red[200],
-                  borderRadius: BorderRadius.only(
-                    // bottomLeft: Radius.circular(40.0),
-                    bottomRight: Radius.circular(40.0),
-                  )),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.accessibility_new_rounded,
-                color: Colors.green[200],
-              ),
-              title: Text('home'),
-              onTap: () {
-                print('home on tap');
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.battery_saver_sharp,
-                color: Colors.green[200],
-              ),
-              title: Text('바뗴리'),
-              onTap: () {
-                print('바ㅔ리 on tap');
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.add_reaction,
-                color: Colors.green[200],
-              ),
-              title: Text('smile'),
-              onTap: () {
-                print('스일 on tap');
-              },
-            ),
-          ],
-        ),
+    // 스낵바
+    final snackBar = SnackBar(
+      content: const Text('Yay! A SnackBar!'),
+      action: SnackBarAction(
+        label: '확인',
+        onPressed: () {
+          // Some code to undo the change.
+        },
       ),
     );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("스낵바"),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: Text(
+              '버튼',
+              style: TextStyle(color: Colors.grey),
+            ),
+            style: ElevatedButton.styleFrom(primary: Colors.amberAccent, elevation: 0),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ));
   }
 }
